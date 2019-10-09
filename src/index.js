8d941691aac0e284913545dfad7bd4dcbd5d3fb0
Pedro Ortiz <pedroortizgm@gmail.com>
2019-10-09 20:17:48 +0200

    Display the location for each move in the format (col, row) in the move history list.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ function Square(props) {
     </button>
   );
 }
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -32,10 +33,12 @@ function calculateWinner(squares) {
 class Board extends React.Component {
 
   renderSquare(i) {
-    return <Square 
+    return (
+    <Square 
       value={this.props.squares[i]} 
       onClick={() => this.props.onClick(i)}
-    />;
+    />
+    );
   }
 
   render() {
@@ -65,9 +68,11 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history : [{
-        squares : Array(9).fill(null),
-      }],
+      history : [
+        {
+          squares : Array(9).fill(null),
+        }
+      ],
       xIsNext : true,
       stepNumber : 0,
 
@@ -75,16 +80,18 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history.splice(0, this.state.stepNumber + 1);
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
-      return ;
+      return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({ 
       history : history.concat([{
         squares : squares, 
+        col : ((i + 1) % 3) == 0 ? 3 : ((i + 1) % 3),
+        row : Math.ceil((i + 1) / 3)
       }]),
       xIsNext : !this.state.xIsNext,
       stepNumber : history.length,
@@ -95,7 +102,7 @@ class Game extends React.Component {
     this.setState({
       stepNumber : step,
       xIsNext : (step % 2) === 0,
-    })
+    });
   }
 
   render() {
@@ -105,7 +112,7 @@ class Game extends React.Component {
 
     const moves = history.map((step, move) => {
       const description = move ?  
-        "Go to move #" + move : 
+        "Go to move #" + move  + "(" + step.row + ", " + step.col + ")": 
         "Go to game start";
       return (
         <li key={move}>
@@ -118,7 +125,7 @@ class Game extends React.Component {
     if (winner) {
       status = "Winner: " + winner + "!"
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
 
     return (
@@ -126,7 +133,8 @@ class Game extends React.Component {
         <div className="game-board">
           <Board 
             squares={current.squares}
-            onClick={(i) => this.handleClick(i)} />
+            onClick={(i) => this.handleClick(i)} 
+          />
         </div>
         <div className="game-info">
           <div>{ status }</div>
